bb083b03780f53a020109660b4913a776073b9b4
Let me Solve her <141553901+LiamTang827@users.noreply.github.com>
2025-09-08 20:43:55 +0800

    Update mcr.cpp
    delete unuse code

--- mcr.cpp
+++ mcr.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 bool isWin(char game[3][3]){
 	bool win = false;
-	if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
+	if (game[0][0] == game[0][1] && game[0][1] == game[0][2] ) win = true;
 	if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true;
 	if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true;
 	// column
@@ -48,4 +48,5 @@ int main(){
 	cout << game[1][0] << " " << game[1][1] << " " << game[1][2] << endl;
 	cout << game[2][0] << " " << game[2][1] << " " << game[2][2] << endl;
 	return 0;
+
 }
